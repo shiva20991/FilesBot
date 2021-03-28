9521f2abdb259fdd01cd0f334fe1c7b591636450
shiva20991 <80451254+shiva20991@users.noreply.github.com>
2021-03-28 19:31:48 +0530

    Update config.php

--- web/config.php
+++ web/config.php
@@ -26,7 +26,6 @@ Thank you for using me <a href="https://github.com/skrtdev/FilesBot">😬</a>
 
 <u><b>you can forward me any media message</b></u>, and <b><i>I might help you to create a PUBlic link</i></b>.
 
-Subscribe ℹ️ @skrtdev if you ❤️ using this bot!
-EOM;
-$GLOBALS["CHECKING_MESSAGE"] = "🤔";
-require_once __DIR__ . "/../vendor/autoload.php";
+Subscribe ℹ️ Tamil movie channel 
+ @tamilrockersvpn
+@trvpn 
